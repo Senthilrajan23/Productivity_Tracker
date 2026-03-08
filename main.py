9c4ed793bef3fa7cfd583159d049752bc5dafce6
Senthilrajan23 <senthilrajan.sandy@gmail.com>
2026-03-08 13:55:25 +0530

    table structure created using sqlalchemy

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-from datetime import datetime
+from datetime import datetime, UTC
 from flask import Flask, render_template
-from sqlalchemy import create_engine, Column, String, Integer, ForeignKey
+from sqlalchemy import create_engine, Column, String, Integer, ForeignKey, DateTime
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.exc import IntegrityError
 
 #creating database
-engine = create_engine('sqlite:///tasks.db', echo=False)
+engine = create_engine('sqlite:///productivity.db', echo=False)
 Base = declarative_base()
 session = sessionmaker(bind=engine)
 session = session()
 
+class Session(Base):
+    __tablename__ = 'sessions'
+    id = Column(Integer, primary_key=True)
+    aux = Column(String, nullable=False)
+    start_time = Column(DateTime, default=lambda: datetime.now(UTC)) #records time when the row is created
+    end_time = Column(DateTime)
+
+Base.metadata.create_all(engine)
 app = Flask(__name__)
 
 @app.route('/')
